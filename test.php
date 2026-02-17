70317bff16a5cec037335be2d27651ca0cac7dd9
2016446-hub <2016446@est.cedesdonbosco.ed.cr>
2026-02-17 16:20:06 -0600

    Add files via upload

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/test.css">
+</head>
+<body>
+    <?php
+        $nombre1="José";
+        $nombre2="María";
+        $alias="";
+        $porcentaje=0;
+
+        $porcentaje+=strlen($nombre1);
+        $porcentaje+=strlen($nombre2);
+
+        if(str_contains($nombre1,"a")){
+            
+        }
+    ?>
+</body>
+</html>
